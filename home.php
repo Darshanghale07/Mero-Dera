2ffe01087d2abb3a056a140552e2647df105745c
Darshanghale07 <132636264+Darshanghale07@users.noreply.github.com>
2023-05-16 18:04:50 +0545

    first

---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Movie Review</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+    <div>
+        <legend> 
+            <img scr ="F:\project VI\htdocs\database\pic.GIF">
+        </legend>
+
+</body>
+</html>
+
